8d0930001f715020d87c3356bd280761ae299bd2
Christian Oerschkes <christian.oerschkes@hotmail.de>
2023-05-26 10:19:59 +0200

    logout the user when user not found in db

--- src/app/shared/application/user-resolver.service.ts
+++ src/app/shared/application/user-resolver.service.ts
@@ -1,12 +1,25 @@
 import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 import { User } from './model/user.model';
+import { NotificationService } from './notification.service';
 import { UserService } from './user.service';
 
 export const UserResolver: ResolveFn<User> = () => {
   let user: User = User.invalid();
+  let auth: AuthService = inject(AuthService);
+  let notification: NotificationService = inject(NotificationService);
   inject(UserService)
     .loadUserForPrincipal()
-    .subscribe((usr) => (user = usr));
+    .subscribe({
+      next: (u) => {
+        if (u === null) {
+          auth.logout();
+          //todo: show popup error!
+        } else {
+          user = u;
+        }
+      },
+    });
   return user;
 };
