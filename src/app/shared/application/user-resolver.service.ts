a0a779cd36bb0782ca91828c8fd152ecfb3491fe
Christian Oerschkes <christian.oerschkes@hotmail.de>
2023-05-26 10:36:56 +0200

    remove unused import

--- src/app/shared/application/user-resolver.service.ts
+++ src/app/shared/application/user-resolver.service.ts
@@ -2,13 +2,11 @@ import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
 import { User } from './model/user.model';
-import { NotificationService } from './notification.service';
 import { UserService } from './user.service';
 
 export const UserResolver: ResolveFn<User> = () => {
   let user: User = User.invalid();
   let auth: AuthService = inject(AuthService);
-  let notification: NotificationService = inject(NotificationService);
   inject(UserService)
     .loadUserForPrincipal()
     .subscribe({
