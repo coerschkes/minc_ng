8b3f4cfb036453d4dee3bab90a931c4b82e8f121
Christian Oerschkes <christian.oerschkes@hotmail.de>
2023-05-26 12:49:06 +0200

    show error message if user not found in db

--- src/app/shared/application/user-resolver.service.ts
+++ src/app/shared/application/user-resolver.service.ts
@@ -3,17 +3,19 @@ import { ResolveFn } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
 import { User } from './model/user.model';
 import { UserService } from './user.service';
+import { NotificationService } from './notification.service';
 
 export const UserResolver: ResolveFn<User> = () => {
   let user: User = User.invalid();
   let auth: AuthService = inject(AuthService);
+  let notification: NotificationService = inject(NotificationService);
   inject(UserService)
     .loadUserForPrincipal()
     .subscribe({
       next: (u) => {
         if (u === null) {
           auth.logout();
-          //todo: show popup error!
+          notification.showError('User not found in database! Contact the admin.');
         } else {
           user = u;
         }
