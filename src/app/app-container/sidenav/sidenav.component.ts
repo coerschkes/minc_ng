9bc19c26f36346d184d773dc9837b88a0f986cc2
Christian Oerschkes <christian.oerschkes@hotmail.de>
2023-05-28 21:03:24 +0200

    hide vertical scrollbars

--- src/app/app-container/sidenav/sidenav.component.ts
+++ src/app/app-container/sidenav/sidenav.component.ts
@@ -9,17 +9,7 @@ import { ChangeDetectorRef, Component } from '@angular/core';
 export class SidenavComponent {
   mobileQuery: MediaQueryList;
 
-  fillerNav = Array.from({ length: 5 }, (_, i) => `Nav Item ${i + 1}`);
-
-  fillerContent = Array.from(
-    { length: 50 },
-    () =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
-  );
+  fillerNav = Array.from({ length: 5 }, (_, i) => `Nav Item of big doom ${i + 1}`);
 
   private _mobileQueryListener: () => void;
 
