c3601352b80c642efad52967b6e6d78a214652ae
Christian Oerschkes <christian.oerschkes@hotmail.de>
2023-05-31 08:05:50 +0200

    add a settings button

--- src/app/app-container/sidenav/sidenav.component.ts
+++ src/app/app-container/sidenav/sidenav.component.ts
@@ -1,6 +1,9 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
+import { User } from 'src/app/shared/application/model/user.model';
+import { UserService } from 'src/app/shared/application/user.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -9,17 +12,26 @@ import { AuthService } from 'src/app/auth/auth.service';
 })
 export class SidenavComponent {
   mobileQuery: MediaQueryList;
+  currentUser: User = User.invalid();
+  sub: Subscription = new Subscription();
 
   private _mobileQueryListener: () => void;
 
   constructor(
     changeDetectorRef: ChangeDetectorRef,
     media: MediaMatcher,
-    private authService: AuthService
+    private authService: AuthService,
+    private userService: UserService
   ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnInit(): void {
+    this.sub = this.userService.userSubject.subscribe((user) => {
+      this.currentUser = user;
+    });
   }
 
   ngOnDestroy(): void {
