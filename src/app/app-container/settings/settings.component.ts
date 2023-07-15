1ca8bc1fd9bec07baaf697dec69b708808fbddcb
Christian Oerschkes <christian.oerschkes@hotmail.de>
2023-07-15 08:37:58 +0200

    show the apikey on the settings page

--- src/app/app-container/settings/settings.component.ts
+++ src/app/app-container/settings/settings.component.ts
@@ -3,7 +3,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { TokenInfoState } from 'src/app/shared/application/api/model/tokeninfo.model';
 import { UserState } from 'src/app/shared/application/model/user.model';
-import { tokenInfoSelector } from 'src/app/store/api/api.selector';
+import { apiKeySelector, tokenInfoSelector } from 'src/app/store/api/api.selector';
 import { userSelector } from 'src/app/store/app/user.selector';
 
 @Component({
@@ -14,16 +14,19 @@ import { userSelector } from 'src/app/store/app/user.selector';
 export class SettingsComponent implements OnInit {
   user$: Observable<UserState> = new Observable<UserState>();
   tokenInfo$: Observable<TokenInfoState> = new Observable<TokenInfoState>();
+  apiKey$: Observable<String> = new Observable<String>();
   panelOpenState = false;
 
   constructor(
     private userStore: Store<{ user: UserState }>,
-    private tokenInfoStore: Store<{ tokenInfo: TokenInfoState }>
+    private tokenInfoStore: Store<{ tokenInfo: TokenInfoState }>,
+    private apiKeyStore: Store<{ apiKey: string }>
   ) {}
 
   ngOnInit(): void {
     this.user$ = this.userStore.select(userSelector);
     this.tokenInfo$ = this.tokenInfoStore.select(tokenInfoSelector);
+    this.apiKey$ = this.apiKeyStore.select(apiKeySelector);
   }
 
   concatenateStringArray(array: string[]): string {
